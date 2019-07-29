1fb330433fa80f9a510fe3fcbc2ffd2e70621bc3
Gabriel Liwerant <gabriel@abrightconcept.com>
2019-07-29 00:14:29 -0400

    Add http library and endpoint with data to component

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,16 +1,47 @@
-import React from "react";
+import React, { Component } from "react";
 
-const UserList = () => {
-  return (
-    <div>
-      <h2>My List</h2>
-      <ul>
-        <li>Foo</li>
-        <li>Bar</li>
-        <li>Baz</li>
-      </ul>
-    </div>
-  );
+import axios from 'axios';
+
+class UserList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      users: []
+    };
+
+    axios.get('https://randomuser.me/api/?results=100')
+      .then(res => this.setState({ users: res.data.results }))
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    const { users } = this.state;
+
+    return (
+      <div>
+        <h2>User Directory</h2>
+        <ul>
+          {users.map((user, index) => (
+            <li key={ index }>
+              <div>
+                <img src={ user.picture.thumbnail } />
+              </div>
+              <div>
+                <div>
+                  { user.name.first } { user.name.last }
+                </div>
+                <div>
+                  <p>{ user.location.street }</p>
+                  <p>{ user.location.city }, { user.location.state }</p>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 };
 
 export default UserList;
